1819ecf6f9adfd56e4a907a17e7d902108cae7ea
Yuriy Kaplun <kaplun.yuriy@gmail.com>
2019-02-25 12:36:41 +0200

    Update OddOccurrences so that it passes the tests

--- Algorithms/Arrays/OddOccurrencesInArray.cs
+++ Algorithms/Arrays/OddOccurrencesInArray.cs
@@ -1,19 +1,25 @@
-﻿namespace Algorithms.Arrays
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Arrays
 {
     public static class OddOccurrencesInArray
     {
         public static int FindOddNumber(int[] array)
         {
-            int length = array.Length;
-            for (int i = 0; i < length; i++)
+            if (array.Length == 1)
+                return array[0];
+
+            var hashSet = new HashSet<int>();
+
+            for (int i = 0; i < array.Length; i++)
             {
-                int nextPairIndex = i + 2;
-                if (nextPairIndex < length)
-                    if (array[i] != array[nextPairIndex])
-                        return array[nextPairIndex];
+                if (hashSet.Contains(array[i]))
+                    hashSet.Remove(array[i]);
+                else hashSet.Add(array[i]);
             }
 
-            return 0;
+            return hashSet.Any() ? hashSet.First() : 0;
         }
     }
 }
